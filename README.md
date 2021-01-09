1ba65c44ec7d6bcd095c8b89ae3131f862e4c821
Yanko Aleksandrov <yanko.aleksandrov@qaiware.com>
2021-01-09 09:49:20 +0200

    Working on registration.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Open Source Ethereum 2.0 Beacon Chain Explorer and validator notifications.
 
 ### URL https://beconcain.uc.r.appspot.com/
+
+### Use 030_sessions/03_signup/main.go
--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
+
+	uuid "github.com/satori/go.uuid"
 )
 
-func main() {
-
-}
+var tpl *template.Template
 
 func init() {
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.ListenAndServe(":8080", nil)
+	tpl = template.Must(template.ParseGlob("templates/*"))
+	// http.Handle("/", http.FileServer(http.Dir(".")))
+	// http.ListenAndServe(":80", nil)
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("http/css"))))
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":80", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+	tpl.ExecuteTemplate(w, "index.gohtml", c.Value)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	return c
 }
